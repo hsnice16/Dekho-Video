69265b717176756df4bae317e49e9b8175dbf8a1
himanshu16singh <hsnice16@gmail.com>
2022-03-25 10:04:03 +0530

    add Chip component and review suggested change

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,31 @@
-import React from "react";
-import { VideoList } from "components";
+import React, { useState } from "react";
+import styles from "./Home.module.css";
+import { Chip, VideoList } from "components";
 
 export const Home = () => {
+  const [activeChip, setActiveChip] = useState("All");
+  const handleChipClick = (selectedChip) => {
+    setActiveChip(selectedChip);
+  };
+
   return (
     <main>
+      <ul
+        className={`align-i-ctr flex py-1p5 px-4 ${styles.CategoryChipContainer}`}
+      >
+        {["All", "Trailers", "Clips", "Movies", "Talks"].map((category) => (
+          <li>
+            <Chip
+              textToShow={category}
+              activeChip={activeChip}
+              handleChipClick={() => {
+                handleChipClick(category);
+              }}
+            />
+          </li>
+        ))}
+      </ul>
+
       <VideoList />
     </main>
   );
